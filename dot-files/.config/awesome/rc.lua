80c293ed6acfd13101c54a7e3369f63e87daab95
Daniel Vartanov <dan@vartanov.net>
2018-09-07 15:58:01 +0100

    Remove system tray

--- dot-files/.config/awesome/rc.lua
+++ dot-files/.config/awesome/rc.lua
@@ -244,7 +244,6 @@ awful.screen.connect_for_each_screen(function(s)
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
             mykeyboardlayout,
-            wibox.widget.systray(),
             mytextclock,
             s.mylayoutbox,
         },
