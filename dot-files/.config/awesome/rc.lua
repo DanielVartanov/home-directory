af01504f938484cdf9ab74af35ac0a1ba666b361
Daniel Vartanov <dan@vartanov.net>
2018-09-07 15:58:22 +0100

    Link certain apps to certain tags

--- dot-files/.config/awesome/rc.lua
+++ dot-files/.config/awesome/rc.lua
@@ -606,3 +606,7 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+awful.spawn("google-chrome", { tag = "1" })
+awful.spawn("emacs-light", { tag = "3" })
+awful.spawn("emacs", { tag = "4" })
